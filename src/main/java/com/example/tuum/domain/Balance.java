6a1fe6a7a1b6683cc83927cd6a769fac33bde037
rakovits <hannaliinarakovits@gmail.com>
2024-04-17 02:41:38 +0300

    feat: add balance object + tests

---
+++ src/main/java/com/example/tuum/domain/Balance.java
@@ -0,0 +1,67 @@
+package com.example.tuum.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Balance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long accountId;
+    private String currency;
+    private Double availableAmount;
+
+
+    public Balance() {
+    }
+
+    public Balance(Long accountId, String currency, Double availableAmount) {
+        this.accountId = accountId;
+        this.currency = currency;
+        this.availableAmount = availableAmount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Double getAvailableAmount() {
+        return availableAmount;
+    }
+
+    public void setAvailableAmount(Double availableAmount) {
+        this.availableAmount = availableAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Balance{" +
+                "id=" + id +
+                ", accountId=" + accountId +
+                ", currency='" + currency + '\'' +
+                ", availableAmount=" + availableAmount +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/example/tuum/repository/BalanceMapper.java
@@ -0,0 +1,29 @@
+package com.example.tuum.repository;
+
+import com.example.tuum.domain.Balance;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface BalanceMapper {
+    @Select("SELECT * FROM balances")
+    List<Balance> getAllBalances();
+
+    @Select("SELECT * FROM balances WHERE id = #{id}")
+    Balance getBalanceById(Long id);
+
+    @Select("SELECT * FROM balances WHERE accountId = #{accountId}")
+    List<Balance> getBalancesByAccountId(Long accountId);
+
+    @Select("SELECT * FROM balances WHERE accountId = #{accountId} AND currency = #{currency}")
+    Balance getBalanceByAccountIdAndCurrency(@Param("accountId") Long accountId, @Param("currency") String currency);
+
+    @Insert("INSERT INTO balances (accountId, currency, availableAmount) VALUES (#{accountId}, #{currency}, #{availableAmount})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void saveBalance(Balance balance);
+
+    @Update("UPDATE balances SET accountId = #{accountId}, currency = #{currency}, availableAmount = #{availableAmount} WHERE id = #{id}")
+    void updateBalance(Balance balance);
+}
+
