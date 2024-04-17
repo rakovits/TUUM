4cac223753807ec7b0be69cd065b016dcd306f92
rakovits <hannaliinarakovits@gmail.com>
2024-04-17 18:57:07 +0300

    feat: add get request to AccountController

--- src/main/java/com/example/tuum/presentation/AccountController.java
+++ src/main/java/com/example/tuum/presentation/AccountController.java
@@ -1,5 +1,31 @@
 package com.example.tuum.presentation;
 
+import com.example.tuum.domain.Account;
+import com.example.tuum.exceptions.AccountNotFoundException;
+import com.example.tuum.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
+        try {
+            Account account = accountService.getAccountById(accountId);
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        } catch (AccountNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
